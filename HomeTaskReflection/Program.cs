9911e82c9673effad75e71ffcfbc64f4a27b6cf1
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-04-12 16:31:08 +0400

    HomeTaskReflection added

---
+++ HomeTaskReflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using HomeTaskReflection;
+
+Student ali = new Student();
+Console.WriteLine(ali.Age); //1
+
+Student murad = new Student();
+Console.WriteLine(murad.Age); //2
+
+Student gadir = new Student();
+Console.WriteLine(gadir.Age); //3
+
+Student nicat = new Student();
+Console.WriteLine(nicat.Age); //4
---
+++ HomeTaskReflection/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskReflection
+{
+    internal class Student
+    {
+        public  byte  Age { get; set; }
+        public static byte Count { get; set; }
+
+        public Student()
+        {
+            Count++;
+            Age=Count;
+        }
+    }
+}
